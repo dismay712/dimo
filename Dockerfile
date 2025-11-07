9e47758594f1ed9b7f55b26d073286b3b996db5d
Znkr <2460064668@qq.com>
2025-11-07 13:28:12 +0000

    2

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,14 @@
 FROM docker:dind
 
+WORKDIR /runner
 
+COPY * .
+
+RUN docker pull gitea:act_runner:0.2.13 && \
+    mkdir data && \
+    chmod +x entrypoint.sh
+
+EXPOSE 2376
+
+CMD [""]
+ENTRYPOINT ["/runner/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env sh
 
-docker pull gitea:act_runner:0.2.13
+cd /runner
+
+sed -e "s#--gitea--#$GITEA_URL#" \
+    -e "s#--token--#$RUNNER_TOKEN#" \
+    -e "s#--rname--#$RUNNER_NAME#" \
+    -i config.yaml
 
 docker compose up -d
