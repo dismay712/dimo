181b63f3cac2b75e12c0a8db7ca56d2d4f8ea26f
ZnXr <2460064668@qq.com>
2025-11-21 19:33:51 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,8 @@ cd /runner && docker compose up -d
 
 curl -o komari -L https://github.com/komari-monitor/komari-agent/releases/latest/download/komari-agent-linux-amd64 && \
 chmod +x komari && \
-screen -dmS Komari ./komari -e $KOMARI_SERVER \
+screen -dmS Komari ./komari $KOMARI_ARGS \
+-e $KOMARI_SERVER \
 -t $KOMARI_SECRET \
 --disable-web-ssh \
 --interval 5.0 \
