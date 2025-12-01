0439f08e6914cf294b65773e8a05022eea8a8de8
ZnXr <2460064668@qq.com>
2025-12-01 17:56:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,14 +10,8 @@ done
 cd /runner && docker compose up -d
 
 curl -o komari -L https://github.com/komari-monitor/komari-agent/releases/latest/download/komari-agent-linux-amd64 && \
-chmod +x komari && \
-screen -dmS Komari ./komari $KOMARI_ARGS \
--e $KOMARI_SERVER \
--t $KOMARI_SECRET \
---disable-web-ssh \
---interval 5.0 \
---max-retries 5 \
---reconnect-interval 10 \
---info-report-interval 15
+chmod +x komari
 
-/runner/tinyhttp
+mkdir -p /etc/supervisor.d && cp /runner/apps.ini /etc/supervisor.d/apps.ini
+
+supervisord -c /etc/supervisord.conf
